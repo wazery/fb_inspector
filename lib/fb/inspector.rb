3f54078da05d88ac7ec3787b76f6f5294debac59
Islam Wazery <wazery@ubuntu.com>
2016-11-09 11:52:45 +0200

    Implmenet multiple post limits

--- lib/fb/inspector.rb
+++ lib/fb/inspector.rb
@@ -4,20 +4,17 @@ require 'koala'
 module Fb
   module Inspector
     class Interactions
-      def initialize(page_ids, post_limit)
-        @page_ids   = page_ids
-        @post_limit = post_limit
+      def initialize(page_ids, posts_limit)
+        @page_ids    = page_ids.delete_if(&:blank?)
+        @posts_limit = posts_limit.delete_if(&:blank?)
       end
 
       def get_interactions
         @graph = Koala::Facebook::API.new(ENV['GRAPH_API_ACCESS_TOKEN'], ENV['GRAPH_API_APP_SECRET'])
         interactions_arr = []
 
-        # Removes empty strings, if any.
-        @page_ids.delete_if(&:blank?)
-
-        @page_ids.each do |page_id|
-          page_feed = @graph.get_connections(page_id, 'feed', limit: @post_limit)
+        @page_ids.each_with_index do |page_id, index|
+          page_feed = @graph.get_connections(page_id, 'feed', limit: @posts_limit[index])
 
           page_feed.each do |post|
             reactions = @graph.get_connections(post['id'], 'reactions')
